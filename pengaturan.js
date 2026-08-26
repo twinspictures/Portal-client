a36c74610d38108aacc182af1092e67d9bdf6d46
twinspictures <twinspictures99@gmail.com>
2026-08-26 14:38:46 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "6282149402202" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
